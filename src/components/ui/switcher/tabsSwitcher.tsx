ea00d2c961633fd21a04482d374803cef6892837
KuleshUladzislau <kulesh_uladzislau@mail.ru>
2023-09-17 20:28:21 +0300

    test swticher

--- src/components/ui/switcher/tabsSwitcher.tsx
+++ src/components/ui/switcher/tabsSwitcher.tsx
@@ -1,11 +1,25 @@
 import { Tabs } from '@radix-ui/themes'
+import s from 'tabsSwitcher.module.scss'
 
-export const TabsSwitcher = () => {
+type TabsProps = {
+  tabsName?: string[]
+  disabled?: boolean
+}
+
+export const TabsSwitcher = ({ tabsName, disabled }: TabsProps) => {
+  // const tabsTrigger = tabsName.map(t => (
+  //   <Tabs.Trigger key={t} value={t}>
+  //     {t}
+  //   </Tabs.Trigger>
+  // ))
+
   return (
-    <Tabs.Root>
-      <Tabs.Trigger value="tabs1"></Tabs.Trigger>
-      <Tabs.Trigger value="tabs1"></Tabs.Trigger>
-      <Tabs.Trigger value="tabs1"></Tabs.Trigger>
+    <Tabs.Root defaultValue="account">
+      <Tabs.List>
+        <Tabs.Trigger value="account">Account</Tabs.Trigger>
+        <Tabs.Trigger value="documents">Documents</Tabs.Trigger>
+        <Tabs.Trigger value="settings">Settings</Tabs.Trigger>
+      </Tabs.List>
     </Tabs.Root>
   )
 }
