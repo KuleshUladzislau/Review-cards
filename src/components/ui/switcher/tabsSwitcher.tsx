5d7a9516326a6c6b6df2f1a536a86a40f402a1fc
KuleshUladzislau <kulesh_uladzislau@mail.ru>
2023-09-17 19:32:08 +0300

    add folder switchTabs

---
+++ src/components/ui/switcher/tabsSwitcher.tsx
@@ -0,0 +1,11 @@
+import { Tabs } from '@radix-ui/themes'
+
+export const TabsSwitcher = () => {
+  return (
+    <Tabs.Root>
+      <Tabs.Trigger value="tabs1"></Tabs.Trigger>
+      <Tabs.Trigger value="tabs1"></Tabs.Trigger>
+      <Tabs.Trigger value="tabs1"></Tabs.Trigger>
+    </Tabs.Root>
+  )
+}
