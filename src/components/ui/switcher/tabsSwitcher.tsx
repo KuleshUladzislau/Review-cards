c5bf68318c461faa98339af871967e1380c2b7ce
KuleshUladzislau <kulesh_uladzislau@mail.ru>
2023-09-17 22:12:54 +0300

    added css styles

--- src/components/ui/switcher/tabsSwitcher.tsx
+++ src/components/ui/switcher/tabsSwitcher.tsx
@@ -1,24 +1,32 @@
-import React, { FormEvent } from 'react'
+import { FC } from 'react'
 
 import * as Tabs from '@radix-ui/react-tabs'
 
+import s from './tabsSwitcher.module.scss'
+
 type TabsProps = {
   tabsName: string[]
-  disabled: boolean
+  disabled?: boolean
+  className?: string
 }
 
-export const TabsSwitcher: React.FC<TabsProps> = ({ tabsName, disabled }: TabsProps) => {
+export const TabsSwitcher: FC<TabsProps> = ({ tabsName, disabled, className }: TabsProps) => {
   const handleTabChange = (value: string) => {
     console.log('Selected value:', value)
     // Вы можете выполнить здесь любую логику, основанную на выбранном значении вкладки
   }
-  const tabs = tabsName.map(t => <Tabs.Trigger key={t} value={t}>{t}</Tabs.Trigger>)
+
+  const classNames = s.default
+
+  const tabs = tabsName.map(t => (
+    <Tabs.Trigger key={t} value={t} className={classNames} >
+      {t}
+    </Tabs.Trigger>
+  ))
 
   return (
     <Tabs.Root defaultValue="account" onValueChange={handleTabChange}>
-      <Tabs.List>
-        {tabs}
-      </Tabs.List>
+      <Tabs.List>{tabs}</Tabs.List>
     </Tabs.Root>
   )
 }
