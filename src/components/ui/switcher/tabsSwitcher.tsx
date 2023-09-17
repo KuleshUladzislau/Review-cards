773c017a86c1f967750c27905f827220ec26b86d
KuleshUladzislau <kulesh_uladzislau@mail.ru>
2023-09-17 21:27:45 +0300

    precomit to update

--- src/components/ui/switcher/tabsSwitcher.tsx
+++ src/components/ui/switcher/tabsSwitcher.tsx
@@ -3,7 +3,7 @@ import React, { FormEvent } from 'react'
 import * as Tabs from '@radix-ui/react-tabs'
 
 type TabsProps = {
-  tabsName: string
+  tabsName: string[]
   disabled: boolean
 }
 
@@ -12,13 +12,12 @@ export const TabsSwitcher: React.FC<TabsProps> = ({ tabsName, disabled }: TabsPr
     console.log('Selected value:', value)
     // Вы можете выполнить здесь любую логику, основанную на выбранном значении вкладки
   }
+  const tabs = tabsName.map(t => <Tabs.Trigger key={t} value={t}>{t}</Tabs.Trigger>)
 
   return (
     <Tabs.Root defaultValue="account" onValueChange={handleTabChange}>
       <Tabs.List>
-        <Tabs.Trigger value="account">Account</Tabs.Trigger>
-        <Tabs.Trigger value="documents">Documents</Tabs.Trigger>
-        <Tabs.Trigger value="settings">Settings</Tabs.Trigger>
+        {tabs}
       </Tabs.List>
     </Tabs.Root>
   )
