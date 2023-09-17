2ac5a2849b9466e61204d1099dc7c5aefd431d4a
Alexandr <alesher696@gmail.com>
2023-09-17 21:00:39 +0300

    Al commit styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import './App.css'
+import { Button } from './components/ui/button'
+import Icon from './styles/assets/icons/icon'
 
 function App() {
-  return <div></div>
+  return (
+    <Button as={'button'} variant={'primary'}>
+      <div>
+        <Icon />
+        Button
+      </div>
+    </Button>
+  )
 }
 
 export default App
