cc0d8276fe359cfc4159876bfdc6fdf9a2083261
KuleshUladzislau <kulesh_uladzislau@mail.ru>
2023-09-17 20:28:21 +0300

    test swticher

---
+++ src/components/ui/switcher/tabsSwitcher.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { TabsSwitcher } from './tabsSwitcher.tsx'
+
+const meta = {
+  title: 'Components/TabsSwitcher',
+  component: TabsSwitcher,
+  tags: ['autodocs'],
+} satisfies Meta<typeof TabsSwitcher>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+
+  },
+}
