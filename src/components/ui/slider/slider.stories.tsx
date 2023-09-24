24201fc7d7f6146c673125422741bea62ed98661
Alexandr <alesher696@gmail.com>
2023-09-24 19:22:58 +0300

    components/changesAlexandr

---
+++ src/components/ui/slider/slider.stories.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+import type { Meta } from '@storybook/react'
+
+import { SliderCustom } from '../slider/slider'
+
+const meta = {
+  title: 'Components/Slider',
+  component: SliderCustom,
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof SliderCustom>
+
+export default meta
+
+export const Default = {
+  render: () => {
+    const min = 0
+    const max = 15
+    const [val, setValues] = useState({ min: 0, max: 15 })
+    const onChangeHandler = (values: number[]) => {
+      setValues({ min: values[0], max: values[1] })
+    }
+
+    return (
+      <SliderCustom
+        min={min}
+        max={max}
+        onValueChange={onChangeHandler}
+        value={[val.min, val.max]}
+        defaultValue={[min, max]}
+      />
+    )
+  },
+}
+
+//storyDocs, args: OnChange
