2147cc684ec7490c8fc8d44ae08382aff6e93335
Alexandr <alesher696@gmail.com>
2023-09-26 13:12:35 +0300

    component/tableAleks

--- src/components/ui/table/table.stories.tsx
+++ src/components/ui/table/table.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react'
 
-import { Table } from './table'
+import { Table, TableHeader, TableBody, TRow, THead, TData } from './table'
+import t from './table.module.scss'
 
 const meta = {
   title: 'Components/Table',
@@ -14,5 +15,23 @@ type Story = StoryObj<typeof meta>
 export const TableDefault: Story = {
   args: {
     children: <></>,
+  },
+}
+
+export const TableDefaults = {
+  render: () => {
+    const tRows = ['Question', 'Answer', 'Last Updated', 'Grade']
+    const tRowsMapped = tRows.map((el, i) => <THead key={i}>{el}</THead>)
+
+    const tData = []
+
+    return (
+      <Table className={t.table}>
+        <TableHeader className={t.tableHead}>
+          <TRow className={t.tableRow}>{tRowsMapped}</TRow>
+        </TableHeader>
+        <TableBody></TableBody>
+      </Table>
+    )
   },
 }
--- src/components/ui/table/table.tsx
+++ src/components/ui/table/table.tsx
@@ -1,21 +1,51 @@
+import { ComponentPropsWithoutRef } from 'react'
+
 import t from './table.module.scss'
 
 type tableType = {
   children: React.ReactNode
+} & ComponentPropsWithoutRef<'table'>
+
+type TableHeadType = {
+  children: React.ReactNode
+} & ComponentPropsWithoutRef<'thead'>
+
+type TableBodyType = {
+  children: React.ReactNode
+} & ComponentPropsWithoutRef<'tbody'>
+
+type TRType = {
+  children: React.ReactNode
+} & ComponentPropsWithoutRef<'tr'>
+
+type THType = {
+  children: React.ReactNode
+} & ComponentPropsWithoutRef<'th'>
+
+type TDType = {
+  children: React.ReactNode
+} & ComponentPropsWithoutRef<'td'>
+
+export const Table: React.FC<tableType> = ({ children, className }) => {
+  return <table className={className}>{children}</table>
 }
 
-export const Table: React.FC<tableType> = ({ children }) => {
-  return (
-    <table>
-      <thead className={t.table}>
-        <tr>
-          <th></th>
-          {/*<th>Cards</th>*/}
-          {/*<th onClick={onSortHandler}>Last Updated</th>*/}
-          {/*<th>Created by</th>*/}
-        </tr>
-      </thead>
-      <tbody>{children}</tbody>
-    </table>
-  )
+export const TableHeader: React.FC<TableHeadType> = ({ children, className }) => {
+  return <thead className={className}>{children}</thead>
+}
+
+export const TableBody: React.FC<TableBodyType> = ({ children }) => {
+  return <tbody>{children}</tbody>
+}
+
+export const TRow: React.FC<TRType> = ({ children, className }) => {
+  return <tr className={className}>{children}</tr>
+}
+
+export const THead: React.FC<THType> = ({ children }) => {
+  return <th>{children}</th>
+}
+
+export const TData: React.FC<TDType> = ({ children }) => {
+  return <td>{children}</td>
 }
