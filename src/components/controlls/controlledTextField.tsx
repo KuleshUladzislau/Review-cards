8ffc160564a07c160d6a3d5da372023182205cbc
KuleshUladzislau <kulesh_uladzislau@mail.ru>
2023-09-27 23:29:17 +0300

    add controlled textField

---
+++ src/components/controlls/controlledTextField.tsx
@@ -0,0 +1,34 @@
+import { FieldValues, useController, UseControllerProps } from 'react-hook-form'
+
+import { Textfield } from '../ui/textfield'
+import type { TextfieldProps } from '../ui/textfield'
+
+export type ControlledTextFieldProps<TFieldValues extends FieldValues> =
+  UseControllerProps<TFieldValues> & Omit<TextfieldProps, 'onChange' | 'value'>
+
+export const controlledTextField = <TFieldValues extends FieldValues>({
+  name,
+  rules,
+  shouldUnregister,
+  control,
+  ...textFieldProps
+}: ControlledTextFieldProps<TFieldValues>) => {
+  const {
+    field: { onChange, value },
+  } = useController({
+    name,
+    control,
+    shouldUnregister,
+    rules,
+  })
+
+  return (
+    <Textfield
+      {...{
+        onChange,
+        value,
+        ...textFieldProps,
+      }}
+    />
+  )
+}
