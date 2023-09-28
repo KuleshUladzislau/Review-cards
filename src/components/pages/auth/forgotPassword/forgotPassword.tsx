0768e4ce63df2ef4797b277ef3442ad38ff1097a
KuleshUladzislau <kulesh_uladzislau@mail.ru>
2023-09-28 16:52:33 +0300

    added verification

--- src/components/pages/auth/forgotPassword/forgotPassword.tsx
+++ src/components/pages/auth/forgotPassword/forgotPassword.tsx
@@ -1,35 +1,58 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { SubmitHandler, useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 import { ControlledTextField } from '../../../controlls'
 import { Button } from '../../../ui/button'
 import { Card } from '../../../ui/card'
 import { Typography } from '../../../ui/typography'
 
+import s from './forgotPassword.module.scss'
+
 export const ForgotPassword = () => {
+  const forgotPasswordSchema = z.object({
+    email: z.string().email(),
+  })
+
   const {
-    register,
     handleSubmit,
     control,
     formState: { errors },
-  } = useForm<{ email: string }>()
+  } = useForm<{ email: string }>({
+    resolver: zodResolver(forgotPasswordSchema),
+  })
 
   const onSubmit: SubmitHandler<{ email: string }> = data => {
-    console.log(data.email)
+    console.log(data)
   }
 
   return (
-    <div>
+    <form onSubmit={handleSubmit(onSubmit)} className={s.formContainer}>
       <Card>
         <Typography variant="h1" as="h1">
           Forgot your password?
         </Typography>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <ControlledTextField name="email" required={true} control={control} />
-          <Button variant="primary" type={'submit'} fullWidth={true}>
-            SignIn
-          </Button>
-        </form>
+
+        <ControlledTextField
+          name="email"
+          control={control}
+          label="Email"
+          errorMessage={errors.email?.message}
+        />
+
+        <Typography variant="overline" as={'label'} className={s.description}>
+          Enter your email address and we will send you further instructions
+        </Typography>
+        <Button variant="primary" type={'submit'} fullWidth={true} className={s.submitButton}>
+          SignIn
+        </Button>
+        <Typography variant="overline" as="div">
+          Did you remember your password?
+        </Typography>
+        <Button variant={'link'} onClick={() => {}}>
+          Try logging in
+        </Button>
       </Card>
-    </div>
+    </form>
   )
 }
