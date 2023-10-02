0bf7a7176157b9426d08974710098056e9232820
KuleshUladzislau <kulesh_uladzislau@mail.ru>
2023-10-02 18:50:37 +0300

    added header

--- src/components/ui/avatar/avatar.tsx
+++ src/components/ui/avatar/avatar.tsx
@@ -2,11 +2,21 @@ import { ComponentPropsWithoutRef } from 'react'
 
 import s from './avatar.module.scss'
 
+import { Typography } from '@/components/ui/typography'
+
 type AvatarProps = {
   name?: string
   size?: ComponentPropsWithoutRef<'img'>['width']
   src?: ComponentPropsWithoutRef<'img'>['src']
+  userName?: string
 }
-export const Avatar = ({ src, size = 36, name }: AvatarProps) => {
-  return <img src={src} className={s.avatar} width={size} height={size} alt={`${name} avatar`} />
+export const Avatar = ({ src, size = 36, name, userName }: AvatarProps) => {
+  return (
+    <>
+      <Typography variant={'subtitle1'} as={'span'} className={s.userName}>
+        {userName}
+      </Typography>
+      <img src={src} className={s.avatar} width={size} height={size} alt={`${name} avatar`} />
+    </>
+  )
 }
--- src/components/ui/header/header.stories.tsx
+++ src/components/ui/header/header.stories.tsx
@@ -1,8 +1,8 @@
 import { Meta, StoryObj } from '@storybook/react'
 
-import Logo from '../../../assets/icons/Logo.tsx'
+
 import { Header } from './header.tsx'
-import { Button } from '../button'
+
 
 const meta = {
   title: 'Components/Header',
@@ -14,47 +14,10 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-  render: args => {
-    return (
-      <div>
-        <Header>
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'space-around',
-              padding: '10px',
-            }}
-          >
-            <Logo />
-            <Button onClick={() => {}} variant={'primary'} as={'button'}>
-              Sign in
-            </Button>
-          </div>
-        </Header>
-        <div style={{ padding: '200px' }}>
-          Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod temper ut labore et
-          dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-          cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est labor. Lorem
-          ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod temper ut labore et
-          dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-          cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est labor. Lorem
-          ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod temper ut labore et
-          dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-          cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est labor. Lorem
-          ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod temper ut labore et
-          dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-          cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est labor.
-        </div>
-      </div>
-    )
+  args: {
+    userName: 'Alexander',
+    isLoggedIn: true,
+    userPhoto: 'https://imgv3.fotor.com/images/gallery/american-anime-stule-naked-man-avatar.jpg',
+    email: 'alexander@mail.ru',
   },
 }
