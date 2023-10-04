92591ff70aaeef8f9de326c071b46bacd10d6e8c
KuleshUladzislau <kulesh_uladzislau@mail.ru>
2023-10-04 18:59:10 +0300

    added routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import './styles/index.scss'
 import * as ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+
 
 import App from './App.tsx'
 
@@ -13,8 +13,6 @@ import '@fontsource/roboto/700.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </Provider>
 )
---
+++ src/routes/router.tsx
@@ -0,0 +1,79 @@
+import {
+  createBrowserRouter,
+  Navigate,
+  Outlet,
+  RouteObject,
+  RouterProvider,
+} from 'react-router-dom'
+
+const publicRoutes: RouteObject[] = [
+  {
+    path: '/login',
+    element: <div>login</div>,
+  },
+  {
+    path: '/signUp',
+    element: <div>Sign Up</div>,
+  },
+  {
+    path: '/forgotPassword',
+    element: <div>Forgot Password</div>,
+  },
+  {
+    path: '/forgotPassword-checkEmail',
+    element: <div>Check Email</div>,
+  },
+  {
+    path: '/createNewPassword',
+    element: <div>Create New Password</div>,
+  },
+  {
+    path: '/editeProfile',
+    element: <div>Edite Profile</div>,
+  },
+]
+
+const privateRoutes: RouteObject[] = [
+  {
+    path: '/',
+    element: <div>hello</div>,
+  },
+  {
+    path: '/main',
+    element: <div>Desc</div>,
+  },
+  {
+    path: '/profile',
+    element: <div>Profile</div>,
+  },
+  {
+    path: '/desc/:id?',
+    element: <div>Desc</div>,
+  },
+  {
+    path: '/cards/:id?',
+    element: <div>cards</div>,
+  },
+  {
+    path: '/learn/:id?',
+    element: <div>learn cards</div>,
+  },
+]
+
+const PrivateRoutes = () => {
+  const isAuthenticated = true
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />
+}
+
+const router = createBrowserRouter([
+  {
+    element: <PrivateRoutes />,
+    children: privateRoutes,
+  },
+  ...publicRoutes,
+])
+
+export const Router = () => {
+  return <RouterProvider router={router} />
+}
