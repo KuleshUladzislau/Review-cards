bbf51b87d1fee3bfcfa35a971a64b69e9637219c
KuleshUladzislau <kulesh_uladzislau@mail.ru>
2023-10-08 17:45:15 +0300

    refactor mutex comment

--- src/services/base-api-query-with-reauth.ts
+++ src/services/base-api-query-with-reauth.ts
@@ -14,12 +14,10 @@ export const baseQueryWithMutex: BaseQueryFn<
   unknown,
   FetchBaseQueryError
 > = async (args, api, extraOptions) => {
-  // wait until the mutex is available without locking it
   await mutex.waitForUnlock()
   let result = await baseQuery(args, api, extraOptions)
 
   if (result.error && result.error.status === 401) {
-    // checking whether the mutex is locked
     if (!mutex.isLocked()) {
       const release = await mutex.acquire()
 
@@ -34,11 +32,9 @@ export const baseQueryWithMutex: BaseQueryFn<
           result = await baseQuery(args, api, extraOptions)
         }
       } finally {
-        // release must be called once the mutex should be released again.
         release()
       }
     } else {
-      // wait until the mutex is available without locking it
       await mutex.waitForUnlock()
       result = await baseQuery(args, api, extraOptions)
     }
