4efb1e301616b7b92b3716410741b57e20a2783f
KuleshUladzislau <kulesh_uladzislau@mail.ru>
2023-10-08 17:34:25 +0300

    added refrech token api

--- src/services/base-api-query-with-reauth.ts
+++ src/services/base-api-query-with-reauth.ts
@@ -1,8 +1,6 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/query'
 import type { BaseQueryFn, FetchArgs, FetchBaseQueryError } from '@reduxjs/toolkit/query'
 
-
-
 const baseQuery = fetchBaseQuery({
   baseUrl: 'https://api.flashcards.andrii.es',
   credentials: 'include',
