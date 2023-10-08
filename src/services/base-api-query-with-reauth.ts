779fee19ea3161a4b48144477297ad6c97284dae
KuleshUladzislau <kulesh_uladzislau@mail.ru>
2023-10-08 17:44:04 +0300

    added mutex

--- src/services/base-api-query-with-reauth.ts
+++ src/services/base-api-query-with-reauth.ts
@@ -1,26 +1,45 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/query'
 import type { BaseQueryFn, FetchArgs, FetchBaseQueryError } from '@reduxjs/toolkit/query'
+import { Mutex } from 'async-mutex'
 
 const baseQuery = fetchBaseQuery({
   baseUrl: 'https://api.flashcards.andrii.es',
   credentials: 'include',
 })
 
-export const baseApiQuery: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (
-  args,
-  api,
-  extraOptions
-) => {
+const mutex = new Mutex()
+
+export const baseQueryWithMutex: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  // wait until the mutex is available without locking it
+  await mutex.waitForUnlock()
   let result = await baseQuery(args, api, extraOptions)
 
   if (result.error && result.error.status === 401) {
-    const refreshResult = await baseQuery(
-      { url: '/v1/auth/refresh-token', method: 'POST' },
-      api,
-      extraOptions
-    )
+    // checking whether the mutex is locked
+    if (!mutex.isLocked()) {
+      const release = await mutex.acquire()
 
-    if (refreshResult?.meta?.response?.status === 204) {
+      try {
+        const refreshResult = await baseQuery(
+          { url: '/v1/auth/refresh-token', method: 'POST' },
+          api,
+          extraOptions
+        )
+
+        if (refreshResult?.meta?.response?.status === 204) {
+          result = await baseQuery(args, api, extraOptions)
+        }
+      } finally {
+        // release must be called once the mutex should be released again.
+        release()
+      }
+    } else {
+      // wait until the mutex is available without locking it
+      await mutex.waitForUnlock()
       result = await baseQuery(args, api, extraOptions)
     }
   }
