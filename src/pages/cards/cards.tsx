ae3e07e2d3baab03fab6bd85ba61e7f47d967323
KuleshUladzislau <kulesh_uladzislau@mail.ru>
2023-10-19 12:12:07 +0300

    added validation photo and  question

--- src/pages/cards/cards.tsx
+++ src/pages/cards/cards.tsx
@@ -146,7 +146,7 @@ export const Cards = () => {
         options={pageSizeOptions}
         portionValue={itemsPerPage.value.toString()}
       />
-      <AddCardModal open={addCardModal} setOpen={onAddCardHandler}/>
+      <AddCardModal open={addCardModal} setOpen={onAddCardHandler} deckId={id}/>
     </div>
   )
 }
